5598c936fd1f0f4d7306a607ac4d6744934482bc
eulious <pc.piro.smz@gmail.com>
2022-12-04 20:56:07 +0900

    メンバーリストを取得

---
+++ src/commands/all_members.ts
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder, ChatInputCommandInteraction } from 'discord.js';
+import DiscordCommand from '../utils/command';
+import fs from "node:fs/promises";
+
+export default class AllMembers extends DiscordCommand {
+    public name = "all_members"
+
+    public data = new SlashCommandBuilder()
+        .setName(this.name)
+        .setDescription('メンバーリストを取得')
+
+    public async chatInputAction(interaction: ChatInputCommandInteraction) {
+        const guild = interaction.client.guilds.cache.get(process.env.TESTGUILDID ?? "")
+        const members = await guild?.members.fetch()
+        let text = "id,name\n"
+        members?.each((member) => text += `${member.id},${member.displayName}\n`)
+        await fs.writeFile("/tmp/members.csv", text, "utf-8")
+        await interaction.reply({ files: ["/tmp/members.csv"] });
+    }
+}
